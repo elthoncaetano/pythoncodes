a569a9d8699859d83588d520c38af8d708d4f807
ECaetano <elthoncaetano@gmail.com>
2018-07-01 15:52:09 -0300

    Modifying IF

--- DailyProblem2.py
+++ DailyProblem2.py
@@ -6,13 +6,13 @@ Given an array of integers, return a new array such that each element at index i
 For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 '''
 
-numbers = [2, 2, 1]
+numbers = [1, 2, 3, 4, 5]
 newNumbers = []
 score = len  (numbers)
 for i in numbers:
     newValue = 1
     for b in range(score):
-        if b != i:
+        if numbers[b] != i:
             newValue *= numbers[b]
     newNumbers.append(newValue)
 print (newNumbers)
